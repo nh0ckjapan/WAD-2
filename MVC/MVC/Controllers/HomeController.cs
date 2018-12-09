7039caaf25b7c070982c07101026450a7d3dfcd7
nh0ckjapan <31547509+nh0ckjapan@users.noreply.github.com>
2018-12-09 23:23:14 +0700

    khoi tao
    khoi tao

---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home    
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration r)
+        {
+            return View(r);
+        }
+        public ActionResult ChangeLanguage(string lang)
+        {
+            new LanguageMang().SetLanguage(lang);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MVC/MVC/MyController.cs
@@ -0,0 +1,28 @@
+﻿using MVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC
+{
+    public class MyController
+    {
+        string lang = null;
+        HttpCookie langCookie = Request.Cookies["culture"];  
+            if (langCookie != null) {  
+                lang = langCookie.Value;  
+            } else {  
+                var userLanguage = Request.UserLanguages;
+    var userLang = userLanguage != null ? userLanguage[0] : "";  
+                if (userLang != "") {  
+                    lang = userLang;  
+                } else {  
+                    lang = LanguageMang.GetDefaultLanguage();  
+                }  
+            }  
+            new LanguageMang().SetLanguage(lang);  
+            return base.BeginExecuteCore(callback, state);  
+        }  
+    }
+}
